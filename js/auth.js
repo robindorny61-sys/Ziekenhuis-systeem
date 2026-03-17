4467658acfa064f75f4f48cea2717084c94b6c1c
robindorny61-sys <robindorny61@gmail.com>
2026-03-17 15:03:45 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,41 +1,19 @@
-/* USERS OPSLAAN */
+import { auth } from "./firebase.js";
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-let users = JSON.parse(localStorage.getItem("users")) || [];
-
-/* REGISTER */
-
-function register(){
-
-let user = document.getElementById("regUser").value;
-let pass = document.getElementById("regPass").value;
-
-if(user === "" || pass === ""){
-alert("Vul alles in");
-return;
+window.register = async ()=>{
+let e = regEmail.value;
+let p = regPass.value;
+await createUserWithEmailAndPassword(auth,e,p);
+alert("Account gemaakt");
 }
 
-users.push({user, pass});
-localStorage.setItem("users", JSON.stringify(users));
-
-alert("Account gemaakt!");
-}
-
-/* LOGIN */
-
-function login(){
-
-let user = document.getElementById("loginUser").value;
-let pass = document.getElementById("loginPass").value;
-
-let found = users.find(u => u.user === user && u.pass === pass);
-
-if(found){
-
-localStorage.setItem("currentUser", user);
-window.location.href = "dashboard.html";
-
-}else{
-alert("Foute login");
-}
-
+window.login = async ()=>{
+let e = loginEmail.value;
+let p = loginPass.value;
+await signInWithEmailAndPassword(auth,e,p);
+location.href="dashboard.html";
 }
